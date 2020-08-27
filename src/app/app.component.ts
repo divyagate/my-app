48f2cb5250e3b6d0912bb068a996bc2a7619c885
divya <gatedivya123@gmail.com>
2020-08-27 03:35:27 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  //url ="https://nbtest.free.beeceptor.com/miner";
+  url="assets/data.json";
+  items=[];
+ 
+  constructor(private http:HttpClient){
+  this.http.get(this.url).toPromise().then(data =>{  // for url I used post method
+  console.log(data['result']);
+
+  for (let item in data['result']){
+ 
+  if(data['result'].hasOwnProperty(item)){
+      this.items.push(data['result'][item]);
+      }
+  }
+   console.log(this.items);
+   });
+  }
 }
